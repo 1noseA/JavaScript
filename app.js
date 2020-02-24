37a752e13f65e4a442bdb1811b9252f12a19d8dd
1noseA <1noseA2L@gmail.com>
2020-02-24 18:28:16 +0900

    [Finish]6-2

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-var orange = 100;
-var apple = 120;
-
-if(orange < apple){
-	alert('みかんの値段がりんごより安い');
-}
-else if(orange == apple){
-	alert('みかんとりんごが同じ値段');
-}
-else{
-	alert('みかんの値段がりんごより高い');
-}
+alert(2+3);
+alert(3-2);
+alert(2*3);
+alert(10/2);
